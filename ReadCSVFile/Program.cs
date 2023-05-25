212ba50045538d63b2fa22ec77fb1cf1a8cfd7eb
Capricorn <Capricorn@DESKTOP-DBG7PJB>
2023-05-25 18:32:58 +0530

    Add project files.

---
+++ ReadCSVFile/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data;
+using Microsoft.VisualBasic.FileIO;
+
+namespace ReadCSVFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String filePath = @"C:\Users\Capricorn\Downloads\Business-employment-data-september-2022-quarter-csv\Business-employment-data-september-2022-quarter-csv.csv";
+            try
+            {
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
+
